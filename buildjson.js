991bcfa49e6e6509ea405bc61ab777b1dc3cb055
Dan Moore <dan@fusionauth.io>
2022-11-19 15:25:21 -0700

    Make sure messages are handled in upload.sh as well as the watch

--- buildjson.js
+++ buildjson.js
@@ -6,16 +6,28 @@ const tmpFolder = './'+process.env.TMP_DIR+'/';
 
 const fs = require('fs');
 const obj = {}
+const theme = {};
 
 fs.readdirSync(tmpFolder).forEach(file => {
+  if (!file.endsWith(".ftl")) {
+    return;
+  }
   let name = file.replace(".ftl","");
   // console.log(name);
   let rawdata = fs.readFileSync(tmpFolder+file);
   obj[name] = String(rawdata)
 });
 
-const theme = {};
 theme['templates'] = obj;
+
+fs.readdirSync(tmpFolder).forEach(file => {
+  if (!file.endsWith(".txt")) {
+    return;
+  }
+  let rawdata = fs.readFileSync(tmpFolder+file);
+  theme["defaultMessages"] = String(rawdata)
+});
+
 const wrapper = {};
 wrapper['theme'] = theme;
 
--- upload.sh
+++ upload.sh
@@ -4,3 +4,4 @@ node buildjson.js > $TMP_DIR/updated.json
 
 curl -XPATCH -H 'Content-type: application/json' -H 'Authorization: '$API_KEY $FUSIONAUTH_URL/api/theme/$THEME_ID -d@$TMP_DIR/updated.json > /dev/null
 
+rm $TMP_DIR/updated.json
