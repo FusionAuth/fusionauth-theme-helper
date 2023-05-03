56e553a8834ffc4c5a13fba533db6cc2be1767e5
Philip Hess <phess@kloveair1.com>
2023-05-03 16:17:13 -0400

    Add chokidar for better file watch performance

--- startwatch.js
+++ startwatch.js
@@ -1,44 +1,57 @@
-const fs = require('fs');
-const https = require('https');
-const dotenv = require('dotenv').config();
+const fs = require("fs");
+const chokidar = require("chokidar");
+const https = require("https");
+const dotenv = require("dotenv").config();
 
 const apiKey = process.env.API_KEY;
 const themeId = process.env.THEME_ID;
 const fusionauthUrl = process.env.FUSIONAUTH_URL;
-const tmpFolder = './'+process.env.TMP_DIR+'/';
+const tmpFolder = "./" + process.env.TMP_DIR + "/";
 
-const {FusionAuthClient, ClientResponse} = require('@fusionauth/typescript-client');
+const {
+  FusionAuthClient,
+  ClientResponse,
+} = require("@fusionauth/typescript-client");
 
 const client = new FusionAuthClient(apiKey, fusionauthUrl);
 
-fs.watch(tmpFolder, (event, filename) => {
+const watcher = chokidar.watch(tmpFolder);
+
+watcher.on("change", (filename) => {
+  filename = filename.split("\\")[1];
   if (filename) {
-    const obj = {}
-    if (! (filename.endsWith(".ftl") || filename.endsWith(".txt") || filename.endsWith(".css")) ) {
+    const obj = {};
+    if (
+      !(
+        filename.endsWith(".ftl") ||
+        filename.endsWith(".txt") ||
+        filename.endsWith(".css")
+      )
+    ) {
       // ignore non template, non css, non text files
       return;
     }
     const theme = {};
     const localizedMessages = {};
 
-    fs.readdirSync(tmpFolder).forEach(file => {
+    fs.readdirSync(tmpFolder).forEach((file) => {
       if (file != filename) {
         return;
       }
 
       // check templates
       if (filename.endsWith(".ftl")) {
-        let name = file.replace(".ftl","");
+        let name = file.replace(".ftl", "");
         // console.log(name);
-        let rawdata = fs.readFileSync(tmpFolder+file);
-        obj[name] = String(rawdata)
+        let rawdata = fs.readFileSync(tmpFolder + file);
+        obj[name] = String(rawdata);
       }
 
       // messages
       if (filename.endsWith(".txt")) {
         // console.log(filename);
-        let name = file.replace(".txt","");
-        let rawdata = fs.readFileSync(tmpFolder+file);
+        let name = file.replace(".txt", "");
+        let rawdata = fs.readFileSync(tmpFolder + file);
         if (name == "defaultMessages") {
           theme["defaultMessages"] = String(rawdata);
         } else {
@@ -50,25 +63,31 @@ fs.watch(tmpFolder, (event, filename) => {
       // check css
       if (filename.endsWith(".css")) {
         // console.log(filename);
-        let rawdata = fs.readFileSync(tmpFolder+file);
-        theme['stylesheet'] = String(rawdata)
+        let rawdata = fs.readFileSync(tmpFolder + file);
+        theme["stylesheet"] = String(rawdata);
       }
     });
-    
-    theme['templates'] = obj;
+
+    theme["templates"] = obj;
 
     const wrapper = {};
-    wrapper['theme'] = theme;
+    wrapper["theme"] = theme;
 
     // console.log(wrapper);
 
-    const toUpload = wrapper; 
-    client.patchTheme(themeId,toUpload).then(clientResponse => {
+    const toUpload = wrapper;
+    client
+      .patchTheme(themeId, toUpload)
+      .then((clientResponse) => {
         if (200 != clientResponse.statusCode) {
-          console.error("ERROR: "+clientResponse);
+          console.error("ERROR: " + clientResponse);
         }
-        console.log("uploaded "+filename);
-    }).catch(console.error);
+        console.log("uploaded " + filename);
+      })
+      .catch(console.error);
   }
 });
 
+process.on("SIGINT", () => {
+  watcher.close();
+});
